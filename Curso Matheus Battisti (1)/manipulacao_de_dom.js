24dbcd5571c2455fe8408f997639fa430159339b
Luiz Madrid <luizam987@gmail.com>
2023-09-16 22:34:38 -0300

    Terminado a sessão de DOM.

--- Curso Matheus Battisti (1)/manipulacao_de_dom.js
+++ Curso Matheus Battisti (1)/manipulacao_de_dom.js
@@ -54,8 +54,74 @@ elementoEmTela.style.backgroundColor = "grey";
 /* ----=========---- ----=========---- ----=========---- ----=========---- */
 
 // Como navegar entre nós(pai, irmão e filho).
-const elementoEmTela1 = document.querySelector("meuInput");
+const elementoEmTela1 = document.querySelector("#meuInput");
 
-const elementoPai = elementoEmTela1.parentNode;
+const elementoPai = elementoEmTela1.parentNode; // Se precisar do elemento "pai" do elemento "pai" acrescentar mais um [.parentNode].
 
 console.log(elementoPai);
+
+/* ----=========---- ----=========---- ----=========---- ----=========---- */
+
+// Manipulando a estrutura com DOM.
+const novoElemento = document.createElement("div");
+console.log(novoElemento);
+
+novoElemento.textContent = "Minha nova div na página com JS.";
+console.log(novoElemento);
+
+document.body.appendChild(novoElemento); // Adiciona dentro do [body] no final.
+document.body.insertBefore(novoElemento, elementoPai); // Adiciona depois na variavel especificada.
+// document.body.removeChild(elementoPorClasse);
+
+/* ----=========---- ----=========---- ----=========---- ----=========---- */
+
+// Eventos - click(), scroll(), submit(), mouseover(), keydown().
+// Click.
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", () => {
+  alert("Botão foi clicado!");
+});
+
+// Mouse.
+const elementoDeFormulario = document.querySelector("#meuFormulario");
+
+elementoDeFormulario.addEventListener("mouseover", () => {
+  console.log("Mouse passou por este campo!");
+});
+
+// Teclado (Digitação).
+const campoDeTeclado = document.querySelector("#meuInput");
+
+campoDeTeclado.addEventListener("keydown", () => {
+  console.log("Teclad neste formulario foram pressionadas.");
+});
+
+// Formulario.
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault;
+
+  console.log("Form enviado!");
+  // Previne que o padrão estabelecido aconteça, no caso do formulario de recarregar a pagina, porem em links ele iria previnir de direcionar o link quando clicado.
+});
+
+// Propagação de eventos.
+document.querySelector("#elementoPai").addEventListener("Click", () => {
+  console.log("Clique capturada no pai.");
+});
+
+document.querySelector("#elementoFilho").addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  console.log("ELemento filho");
+  // O stopPropagation() faz o elemento pai não ser acessado, ao contratio do exemplo acima onde se não fosse atribuido isto ele entraria no elemento pai, pegaria o elemento filho, e pegava o elemento pai também.
+});
+
+document.querySelector("#meuLink").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  console.log("O evento de re-direcionar para o link foi parado.");
+});
+/* ----=========---- ----=========---- ----=========---- ----=========---- */
