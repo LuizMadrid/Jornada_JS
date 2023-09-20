e9f519ff0163232616a64800612500e12f500958
Luiz Madrid <luizam987@gmail.com>
2023-09-20 13:21:18 -0300

    Atualização de arquivos.

---
+++ Curso JS (Curso em Video)/Aula11/ex008.js
@@ -0,0 +1,11 @@
+console.log("O console está funcionando normalmente.");
+console.log("Vamos fazer um simples print com uma condição simples.");
+var pw_bat = 16;
+console.log(`Seu poder de luta é de: ${pw_bat} mil!!`);
+if (pw_bat > 8) {
+  console.log("Parabéns você é um guerreiro de alta classe, derrotou Vegeta.");
+} else {
+  console.log(
+    `Meu deus com míseros ${pw_bat} mil de poder de luta Vegeta te matou com um Galick Ho`
+  );
+}
